4481f458d4f9c7d812ba26e10111485a50b3030a
Chouchoucaicai <1806485592@qq.com>
2022-07-26 12:39:47 +0800

    导航栏

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,18 @@ import Vue from 'vue'
 import Router  from 'vue-router'
 Vue.use(Router )
 import Home from "@/views/Home"
+import Detail from "@/views/Detail"
 const router = new Router ({
   routes: [
     {
       name:'Home',
       path:"/",
       component:Home
+    },
+    {
+      name: "Detail",
+      path: "/detail/:id",
+      component:Detail
     }
   ]
 })
