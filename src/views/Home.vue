d1069251f27a8f3dfed4daa52c86e0c0cfd6f12f
Chouchoucaicai <1806485592@qq.com>
2022-07-26 19:56:27 +0800

    一级菜单更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
+    <Nav></Nav>
     homes
   </div>
 </template>
 <script>
+import Nav from '@/components/Nav.vue';
 
 
 export default {
-  
+    components: { Nav }
 }
 </script>
 
