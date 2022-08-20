0fdf92963bbba1baafca68513eeba8acefc4262c
董泽平 <dzp19903@163.com>
2022-08-20 22:00:43 +0800

    fix:目录修复

--- src/components/Group.vue
+++ src/components/Group.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="catalog-box">
+    <div class="group">
         <div class="title-box">目录</div>
         <div class="item-box" id="item-box">
-            <div v-for="item in catalogData" :key="item.id" :class="['item', item.isActivate ? 'activate' : '', 'level-' + item.level]" @click="goto(item)">
+            <div v-for="item in list" :key="item.id" :class="['item', item.isActivate ? 'activate' : '', 'level-' + item.level]" @click="toItem(item)">
                 <div class="label"></div>
                 <div class="text">{{item.title}}</div>
             </div>
@@ -14,21 +14,21 @@
     export default {
         data() {
             return {
-                catalogData: [],
+                list: [],
                 busy: false,
-                allItem: 0,
-                currItem: 0,
+                total: 0,
+                current: 0,
                 begin:1
             }
         },
         mounted() {
-            let passage = document.querySelector('.container')
-            console.log(passage)
-            let headers = Array.from(passage.querySelectorAll("h3, h4")); //获取所有标签为h1，h2的节点
+            let container = document.querySelector('.container')
+
+            let headers = Array.from(container.querySelectorAll("h2, h3"));
             console.log(headers)
             headers.forEach((item, index) => {
               item.setAttribute("id",'id'+this.begin)
-                this.catalogData.push({
+                this.list.push({
                         title: item.innerHTML,
                         id: "id"+this.begin,
                         level: Number.parseInt(item.localName.slice(1)),
@@ -37,104 +37,75 @@
                     })
                     this.begin++
             });
-            console.log(headers)
-            this.allItem = this.catalogData.length;
-            console.log(this.allItem)
-            window.addEventListener('scroll', this.scrollListener)
+            this.total = this.list.length;
+            window.addEventListener('scroll', this.scrollPage)
         },
         beforeDestory() {
-            window.removeEventListener("scroll", this.scrollListener)
+            window.removeEventListener("scroll", this.scrollPage)
         },
         methods: {
-            goto(item) {
+            toItem(item) {
                 const windowTop = document.body.getBoundingClientRect().top;
-                console.log(windowTop)
-                console.log(item.index)
                 const toTopDistance = document.getElementById(item.id).getBoundingClientRect().top;
-                console.log(toTopDistance)
                 window.scrollTo({
-                top: toTopDistance - windowTop,
-                behavior: 'smooth'
+                  top: toTopDistance - windowTop,
+                  behavior: 'smooth'
                 })
-                this.currItem = item.index;
+                this.current = item.index;
                 this.scrollListener();
             },
-            scrollListener() {
-                //不能一个劲监听
+            scrollPage() {
                 if (this.busy === true) {
                     return;
                 }
-                this.busy = true;
-
-                let passage = document.querySelector('.container')
-                // console.log(passage)
-
-                let flag = false;//如果是false，下拖，否则上拖
-                //先判断是上移了还是下移了
+                this.busy = true
+                let container = document.querySelector('.container')
+                let flag = false;
                 const newWindowBoundTop = document.body.getBoundingClientRect().top;
-                if(this.windowBoundTop > newWindowBoundTop){
-                    //往下拖了
-                    console.log('下拖');
+                if(this.windowBoundTop > newWindowBoundTop)         
                     flag = false;
-                }else{
-                    //往上拖了
-                    console.log('上拖');
+                else
                     flag = true;
-                }
                 this.windowBoundTop = newWindowBoundTop;
-
-                let headers = Array.from(passage.querySelectorAll("h3, h4")); //获取所有标签为h1，h2的节点
-                // console.log(headers)
+                let headers = Array.from(container.querySelectorAll("h2, h3")); //获取所有标签为h1，h2的节点
                 let allNum = headers.length;
                 for (let i = 0; i < allNum; i++) {
-                    console.log(headers[i].getBoundingClientRect().top)
                     if (headers[i].getBoundingClientRect().top > 110 && headers[i].getBoundingClientRect().top < 130) {
-                        console.log("1111111111111111111")
-                        this.catalogData.forEach(item => item.isActivate = false)
+                        
+                        this.list.forEach(item => item.isActivate = false)
                         let indexOfActivate = i;
                         if(flag){
-                            //往上托，如果是0的话，那也只能是0
                             indexOfActivate = (i === 0) ? 0 : i - 1;
                         }
-                        // document.getElementsByClassName('el-tabs__active-bar is-right')[0].setAttribute('style', `height: 40px; transform: translateY(${actualHighlightTitle*40}px);`);
-                        this.catalogData[indexOfActivate].isActivate = true
-                        this.currItem = indexOfActivate;
+                        this.list[indexOfActivate].isActivate = true
+                        this.current = indexOfActivate;
                         break;
                     }
                 }
-
-                //下面实现：active项始终显示在可视区内
-                //前5个，后5个，距离锁死就可以了
                 let itemBox = document.getElementById('item-box');
-                if(this.currItem < 5){
+                if(this.current < 5){
                     itemBox.scrollTop = 0;
-                }else if(this.currItem > this.allItem - 5){
-                    itemBox.scrollTop = 45 * (this.allItem - 5);
+                }else if(this.current > this.total - 5){
+                    itemBox.scrollTop = 45 * (this.total - 5);
                 }else{
-                    itemBox.scrollTop = 45 * (this.currItem - 5);
+                    itemBox.scrollTop = 45 * (this.current - 5);
                 }
-                
                 this.busy = false;
             },
         },
-        watch: {
-
-        }
+       
     }
 </script>
 
 <style  scoped>
-* {
-  box-sizing: border-box;
-}
-.catalog-box {
+.group {
   margin-top: 20px;
   width: 300px;
   background-color: #FFFFFF;
   display: flex;
   flex-direction: column;
 }
-.catalog-box .title-box {
+ .title-box {
   height: 55px;
   line-height: 55px;
   border-bottom: 1px solid #e4e6eb;
@@ -143,12 +114,12 @@
   color: #1d2129;
   margin: 0 20px;
 }
-.catalog-box .item-box {
+ .item-box {
   height: 360px;
   overflow-y: scroll;
   scrollbar-face-color: #E4E6EB;
 }
-.catalog-box .item-box .item {
+.item-box .item {
   width: 100%;
   height: 45px;
   background-color: #FFFFFF;
@@ -156,33 +127,39 @@
   justify-content: space-between;
   align-items: center;
 }
-.catalog-box .item-box .item .label {
+ .item-box .item .label {
   width: 4px;
   height: 16px;
   border-radius: 0 4px 4px 0;
   left: 0;
   background-color: #FFF;
 }
-.catalog-box .item-box .item .text {
+ .item-box .item .text {
   width: 100%;
 }
-.catalog-box .item-box .item.level-1 .text {
+ .item-box .item.level-1 .text {
   color: #000;
   padding-left: 16px;
 }
-.catalog-box .item-box .item.level-2 .text {
+ .item-box .item.level-2 .text {
   color: #333;
   padding-left: 30px;
 }
-.catalog-box .item-box .item.activate {
+ .item-box .item.activate {
   color: #1E80FF;
 }
-.catalog-box .item-box .item.activate .label {
+ .item-box .item.activate .label {
   background-color: #1E80FF;
 }
-.catalog-box .item-box .item:hover {
+ .item-box .item:hover {
   cursor: pointer;
   background-color: #F7F8FA;
+}
+.activate{
+  color: #1E80FF;
+}
+.level-3{
+  margin-left: 20px;
 }
 
 </style>
